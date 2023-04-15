9b5da010c6b6467f328ebf9302ba1b1bd0e0b5d0
Kiriko-I <124024707+Kiriko-I@users.noreply.github.com>
2023-04-15 08:17:32 +0000

    [Add]registration_controllerと関係するビューの編集

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,18 +1,18 @@
 class Public::CustomersController < ApplicationController
-
+  
   def show
   end
-
+  
   def edit
   end
-
+  
   def update
   end
-
+  
   def confirm_deleted
   end
-
+  
   def is_deleted
   end
-
+  
 end
