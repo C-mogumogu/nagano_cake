3f17cae356e293d1252018f61307ad921aa93a6a
Kiriko-I <124024707+Kiriko-I@users.noreply.github.com>
2023-04-16 06:34:51 +0000

    [Finish]会員登録、ログインログアウト

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,18 +1,22 @@
 class Public::CustomersController < ApplicationController
-  
+  before_action :authenticate_customer!
+
   def show
+    @customer = current_customer
   end
-  
+
   def edit
+    @customer = current_customer
   end
-  
+
   def update
   end
-  
+
   def confirm_deleted
+    @customer = current_customer
   end
-  
+
   def is_deleted
   end
-  
+
 end
