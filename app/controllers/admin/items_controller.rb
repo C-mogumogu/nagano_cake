b6089c9777d6b8bde3c890da0d435f7ee121f80a
purupuruputtyan <pika0404_minako@yahoo.co.jp>
2023-04-16 03:20:35 +0000

    [Fix]商品機能編集途中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -20,6 +20,11 @@ class Admin::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    if @item.sell_status == "0"
+      @sell_status = Item.sell_statuses_i18n[:sold_out]
+    else
+      @sell_status = Item.sell_statuses_i18n[:now_on_sale]
+    end
   end
 
   def edit
