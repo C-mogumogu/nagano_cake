cc4a89a4e3fa8c9ac9bb8ef191c981f38c858920
purupuruputtyan <pika0404_minako@yahoo.co.jp>
2023-04-16 10:56:40 +0000

    [Fix]商品編集画面

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,16 +3,16 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
-  enum sell_statuses: { sold_out: 0, now_on_sale: 1 }
+  enum sell_status: { sold_out: 0, now_on_sale: 1 }
 
   def get_image
     (image.attached?) ? image : 'no_image.jpg'
   end
-  
+
   def with_tax_price
     (price * 1.1).floor
   end
-  
+
   has_many :cart_items, dependent: :destroy
 
 end
