41e561c6104b22e57d37b3e4af3f324a6744c55f
EC2 Default User <ec2-user@ip-172-31-40-80.ap-northeast-1.compute.internal>
2023-04-20 04:34:09 +0000

    [finish]admin_order

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -2,15 +2,25 @@ class Admin::OrderItemsController < ApplicationController
   before_action :authenticate_admin!
 
   def update
-     @order_item = OrderItem.find(params[:id])
+    @order_item = OrderItem.find(params[:id])
+    @order = Order.find(params[:id])
+    #@order_items = @order.order_items.all
+
+    #is_updated = true
     if @order_item.update(order_item_params)
-      redirect_to admin_order_path(@order_item.customer_id), notice: "製作ステータスを変更しました"
-    else
-      render 'index'
+       #@order.update(status: 2) if @order_item.making_status == "in_production"
+       #@order_items.each do |order_item|
+        #if order_item.making_status != "production_complete"
+          #is_updated = false
+       # end
+      #end
+      #@order.update(status: 3) if is_updated
+    #end
+    redirect_to request.referer, notice: "製作ステータスを変更しました"
     end
   end
 
-private
+  private
   def order_item_params
     params.require(:order_item).permit(:making_status)
   end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,21 +7,23 @@ class Admin::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    @order_items = @order.order_items
+    @order_items = OrderItem.where(order_id: params[:id])
   end
 
   def update
     @order = Order.find(params[:id])
-    @order_items = @order.order_items
+    @order_items = OrderItem.where(order_id: params[:id])
     if @order.update(order_params)
-      redirect_to admin_order_path(@order), notice: "注文ステータスを変更しました"
+       #@order_items.update_all(making_status: 1)
+       #if @order.status == "payment_confirmation"
+       #end
+       redirect_to admin_order_path(@order), notice: "注文ステータスを変更しました"
     else
       render 'show'
     end
   end
 
   private
-
   def order_params
     params.require(:order).permit(:status)
   end
