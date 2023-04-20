ed5f8eeac7836cf8dfcafd6abd3a71d9a9ec78f2
EC2 Default User <ec2-user@ip-172-31-40-80.ap-northeast-1.compute.internal>
2023-04-20 06:05:22 +0000

    [Finish]admin_status

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -3,21 +3,20 @@ class Admin::OrderItemsController < ApplicationController
 
   def update
     @order_item = OrderItem.find(params[:id])
-    @order = Order.find(params[:id])
-    #@order_items = @order.order_items.all
+    @order = Order.find(@order_item.order_id)
+    @order_items = @order.order_items.all
 
-    #is_updated = true
+    is_updated = true
     if @order_item.update(order_item_params)
-       #@order.update(status: 2) if @order_item.making_status == "in_production"
-       #@order_items.each do |order_item|
-        #if order_item.making_status != "production_complete"
-          #is_updated = false
-       # end
-      #end
-      #@order.update(status: 3) if is_updated
-    #end
-    redirect_to request.referer, notice: "製作ステータスを変更しました"
+      @order.update(status: 2) if @order_item.making_status == "in_production"
+      @order_items.each do |order_item|
+        if order_item.making_status != "production_complete"
+          is_updated = false
+        end
+      end
+      @order.update(status: 3) if is_updated
     end
+    redirect_to request.referer, notice: "製作ステータスを変更しました"
   end
 
   private
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -14,9 +14,11 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_items = OrderItem.where(order_id: params[:id])
     if @order.update(order_params)
-       #@order_items.update_all(making_status: 1)
-       #if @order.status == "payment_confirmation"
-       #end
+       if @order.status == "payment_confirmation"
+          @order_items.update_all(making_status: 1)
+       elsif @order.status == "delivered"
+          @order_items.update_all(making_status: 3)
+       end
        redirect_to admin_order_path(@order), notice: "注文ステータスを変更しました"
     else
       render 'show'
