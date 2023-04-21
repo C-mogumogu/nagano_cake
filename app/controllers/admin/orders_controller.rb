e9982b4ec5d880fc85eb19082b913c7b595e8c9b
Kiriko-I <124024707+Kiriko-I@users.noreply.github.com>
2023-04-21 09:44:22 +0000

    [Finish]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,10 +1,6 @@
 class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
 
-  def index
-    @orders = Order.page(params[:page])
-  end
-
   def show
     @order = Order.find(params[:id])
     @order_items = OrderItem.where(order_id: params[:id])
